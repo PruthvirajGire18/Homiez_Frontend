b7fce1b19594dfa2fdb30a5242ca52e1071e3a77
PruthvirajGire18 <girepruthviraj@gmail.com>
2026-02-25 18:22:05 +0530

    fix: tailwind setup for vite build

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://homiez-backend.vercel.app/api",
+  baseURL: "https://homiez18.netlify.app/api",
   withCredentials: true, // 🔥 MUST for cookies
 })
 
