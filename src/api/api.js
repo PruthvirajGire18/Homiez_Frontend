06d628e345f51d1322ab95dc37742348d20a2f4d
PruthvirajGire18 <girepruthviraj@gmail.com>
2026-02-25 16:28:58 +0530

    base url changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://homiez-backend.vercel.app/api",
   withCredentials: true, // 🔥 MUST for cookies
 })
 
